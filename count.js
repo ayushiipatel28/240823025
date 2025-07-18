780efd6ccbc8010a2655a01872fdac6c691b3346
ayushipatel28 <ayushigondha@gmail.com>
2025-07-18 11:14:59 +0530

    node

---
+++ count.js
@@ -0,0 +1,7 @@
+function countDigits(num) {
+    return num.toString().length;
+  }
+  
+  let num = 5023;
+  console.log(countDigits(num)); 
+  
---
+++ prime.js
@@ -0,0 +1,5 @@
+function isPrime(num) {
+    if (num <= 1) return console.log(' not Prime');
+    else return console.log('Prime');
+  }
+  isPrime(11);

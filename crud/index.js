1353edbdf255c9a1a6769087327eab068669967e
ayushiipatel28 <ayushigodha@gmail.com>
2025-08-08 11:18:17 +0530

    node

--- crud/index.js
+++ crud/index.js
@@ -11,3 +11,19 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}/`);
 });
+
+app.get("/show/:id", (req,res) => {
+    res.send(`helloooo student \n ID:${req.params.id}`);
+});
+
+app.post("/store", (req,res) => {
+    res.send(`successfully stored record \n ID:${req.body.name},${req.body.city}`);
+});
+
+app.put("/update/:id", (req,res) => {
+    res.send(`successfully update record \n ID:${req.params.id},Name:${req.body.name},${req.body.city}`);
+});
+
+app.delete("/delete/:id", (req,res) => {
+    res.send(`successfully deleted record with ID:${req.params.id}`);
+});
